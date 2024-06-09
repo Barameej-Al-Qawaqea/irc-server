b191cf06250890e90b037fefebc41c6fb1f1e32b
rghouzra <rghouzra@student.1337.ma>
2024-06-09 04:30:35 +0100

    update

---
+++ srcs/ClientCommand.cpp
@@ -0,0 +1,6 @@
+/*
+        KICK    - Eject a client from the channel
+        MODE    - Change the channel's mode
+        INVITE  - Invite a client to an invite-only channel (mode +i)
+        TOPIC   - Change the channel topic in a mode +t channel
+*/
