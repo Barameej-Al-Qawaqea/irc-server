ee87553e4ee3b6a141a139ee4e7351e78e8ccd51
achraf <achrafsettar8@gmail.com>
2024-09-05 16:40:51 +0100

    fix error messages

--- include/Commands.hpp
+++ include/Commands.hpp
@@ -2,25 +2,7 @@
 
 #include "header.hpp"
 #include "Client.hpp"
-// #include "Channel.hpp"
-
-//PASS errors
-#define ERR_NEEDMOREPARAMS 461
-#define ERR_ALREADYREGISTRED 462
-#define ERR_PASSWDMISMATCH 464
-//NICK erros
-#define ERR_NONICKNAMEGIVEN 431
-#define ERR_ERRONEUSNICKNAME 432
-#define ERR_NICKNAMEINUSE 433
-#define ERR_NOTREGISTERED 451
-// privmsg
-#define ERR_NORECIPIENT 411
-#define ERR_NOTEXTTOSEND 412
-#define ERR_TOOMANYTARGETS 407
-#define ERR_NOSUCHNICK 401
-
-#include "header.hpp"
-
+#include "Replies.hpp"
 
 Channel *findChan(std::string name, std::deque<Channel *> channels,
                   bool &created);
@@ -33,45 +15,11 @@ class Command
         Client *client;
         s_server_data &serverData;
 
-        static std::string error(int errorNumber, std::string nickName) {
-            std::string serverMsg = ":server ";
-            switch (errorNumber) {
-                case ERR_NEEDMOREPARAMS :
-                    serverMsg += std::to_string(ERR_NEEDMOREPARAMS) + ' ' + nickName + " :Not enough parameters\n";
-                    break;
-                case ERR_ALREADYREGISTRED :
-                    serverMsg += std::to_string(ERR_ALREADYREGISTRED) + ' ' + nickName + " :You may not reregister\n";
-                    break;
-                case ERR_PASSWDMISMATCH :
-                    serverMsg += std::to_string(ERR_PASSWDMISMATCH) + ' ' + nickName + " :Password incorrect\n";
-                    break;
-                case ERR_NONICKNAMEGIVEN :
-                    serverMsg += std::to_string(ERR_NONICKNAMEGIVEN) + ' ' + nickName + " :No nickname given\n";
-                    break;
-                case ERR_ERRONEUSNICKNAME :
-                    serverMsg += std::to_string(ERR_NONICKNAMEGIVEN) + ' ' + nickName + " :Erroneous nickname\n";
-                    break;
-                case ERR_NICKNAMEINUSE :
-                    serverMsg += std::to_string(ERR_NONICKNAMEGIVEN) + ' ' + nickName + " :Nickname is already in use\n";
-                    break;
-                case ERR_NOTREGISTERED :
-                    serverMsg += std::to_string(ERR_NOTREGISTERED) + ' ' + nickName + " :You have not registered\n";
-                case ERR_NORECIPIENT :
-                    serverMsg += "411 " + nickName + " :No recipient given\n";
-                    break;
-                case ERR_NOSUCHNICK :
-                    serverMsg += "401 " + nickName + " :No such nick/channel\n";
-                    break;
-            }
-            return serverMsg;
-        }   
-
         static bool validNickName(std::string &name) {
             std::string validCharacters = "-{}[]^`\\";
             bool validName = 1;
             for(size_t i = 0; i < name.size(); i++)
                 validName &= (std::isalpha(name[i]) || validCharacters.find(name[i]) != std::string::npos);
-            // std::cout << validName << ' ' << name << '\n';
             return validName;
         }
 
@@ -80,7 +28,7 @@ class Command
             std::string destination = client->getNickName().empty() ? "*" : client->getNickName();
             int sendReturn = 1;
             if (!client->isAlreadyRegistred())
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NOTREGISTERED, destination)) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NOTREGISTERED(destination)) != -1;
             else if (client->argumentsError(cmd))
                 sendReturn &= sendMsg(client->getSocket(), client->botUsage()) != -1;
             else sendReturn &= sendMsg(client->getSocket(), client->play(cmd)) != -1;
@@ -91,13 +39,13 @@ class Command
         void    executePass() {
             int sendReturn = 1;
             if (client->isAlreadyRegistred())
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_ALREADYREGISTRED, client->getNickName())) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_ALREADYREGISTRED(client->getNickName())) != -1;
             else if (cmd.size() == 1) {
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NEEDMOREPARAMS, "*")) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NEEDMOREPARAMS((string)"*", (string)"PASS")) != -1;
                 client->unsetAuthenticated();  // only last pass command is used for verif
             }
             else if (cmd.size() > 2 || cmd[1] != serverData.password) {
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_PASSWDMISMATCH, "*")) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_PASSWDMISMATCH((string)"*")) != -1;
                 // this is according to rfc1459, only last pass command is used for verification
                 client->unsetAuthenticated(); 
             }
@@ -109,16 +57,14 @@ class Command
         void    executeNick() {
             int sendReturn = 1;
             std::string destination = client->getNickName().empty() ? "*" : client->getNickName();   // destination should be '*' if user doenst have a nickname yet
-            if (cmd.size() == 1){
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NONICKNAMEGIVEN, destination)) != -1;
-                std::cout << "debug \n";
-            }
+            if (cmd.size() == 1)
+                sendReturn &= sendMsg(client->getSocket(), ERR_NONICKNAMEGIVEN(destination)) != -1;
             else if (cmd.size() > 2 || !validNickName(cmd[1]))
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_ERRONEUSNICKNAME, destination)) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_ERRONEUSNICKNAME(destination)) != -1;
             else if (serverData.clientsNicknames.count(cmd[1])) // already inuse
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NICKNAMEINUSE, destination)) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NICKNAMEINUSE(destination)) != -1;
             else if (!client->getAuthenticated()) // no PASS YEt
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NOTREGISTERED, destination)) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NOTREGISTERED(destination)) != -1;
             else {
                 if (serverData.clientsNicknames.count(client->getNickName()))
                     serverData.clientsNicknames.erase(client->getNickName());
@@ -137,20 +83,17 @@ class Command
 
         void    executeUser() {
             int sendReturn = 1;
-            (void)sendReturn;
             std::string destination = client->getNickName().empty() ? "*" : client->getNickName();
-            std::cout << client->isAlreadyRegistred() << '\n';
             if (cmd.size() != 5)
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NEEDMOREPARAMS, destination));
+                sendReturn &= sendMsg(client->getSocket(), ERR_NEEDMOREPARAMS(destination, "USER"));
             else if (client->isAlreadyRegistred()) 
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_ALREADYREGISTRED, destination));
+                sendReturn &= sendMsg(client->getSocket(), ERR_ALREADYREGISTRED(destination));
             else if (!client->getAuthenticated() || client->getNickName().empty())  // user didnt make a PASS,NICK yet
-                sendReturn &= sendMsg(client->getSocket(),  error(ERR_NOTREGISTERED, destination));
+                sendReturn &= sendMsg(client->getSocket(),  ERR_NOTREGISTERED(destination));
             else {
                 // all good
                 client->setRegistred();
                 sendMsg(client->getSocket(),  "Welcome " + client->getNickName() + " to ft_irc server\n");
-                // todo
                 serverData.nameToClient[client->getNickName()] = client;
             }   
         }
@@ -179,11 +122,21 @@ class Command
         bool clientExist(std::string &name) {
             return serverData.nameToClient.count(name);
         }
-        // prvmsg  client1,clinet2,..,channel1,... :message
+
+        bool    duplicateExist(std::vector<std::string> &toSend) {
+            std::set <std::string> targets;
+            for (size_t i = 0; i < toSend.size(); i++) {
+                if (targets.count(toSend[i])) return 1;
+                targets.insert(toSend[i]);
+            }
+            return 0;
+        }
+        
         bool    sendPrivMessage(std::vector<std::string> &toSend, std::string &message) {
             if (toSend.size() > 10 || !toSend.size() || message.empty()) return 1;
-            // std::cout << ":: " << toSend.size() << '\n';
             bool sendReturn = 1;
+            if (duplicateExist(toSend))
+                return sendMsg(client->getSocket(), ERR_TOOMANYTARGETS(client->getNickName(), originCmd, 1)) != -1;
             for(size_t i = 0; i < toSend.size(); i++) {
                 std::string receiver = toSend[i];
                 if (isChannel(receiver)) {
@@ -191,15 +144,15 @@ class Command
                     bool created = 1;
                     Channel *chan = findChan(receiver.substr(1), serverData.channels, created);
                     if (!created)
-                        sendReturn &= sendMsg(client->getSocket(), error(ERR_NOSUCHNICK, client->getNickName()));
+                        sendReturn &= sendMsg(client->getSocket(), ERR_NOSUCHNICK(client->getNickName(), receiver)) != -1;
                     else {
                         // check if clinet is in channel  : ERR_CANNOTSENDTOCHAN
                         if(!chan->isOnChan(*client))
                             //! ERR_CANNOTSENDTOCHAN
-                            sendReturn &= sendMsg(client->getSocket(), error(500, client->getNickName()));
+                            sendReturn &= sendMsg(client->getSocket(), ERR_CANNOTSENDTOCHAN(client->getNickName(), receiver)) != -1;
                         else {
                             std::vector<Client> ChanClien = chan->getChanClients();
-                            for(int i = 0; i < ChanClien.size(); i++) {
+                            for(size_t i = 0; i < ChanClien.size(); i++) {
                                 if (ChanClien[i].getSocket() == client->getSocket()) continue;    
                                 sendReturn &= sendMsg(ChanClien[i].getSocket(), "Message from " + client->getNickName() + ": " + message + "\n") != -1;
                             }
@@ -213,12 +166,21 @@ class Command
                         sendReturn &= sendMsg(receiverFd, "Message from " + client->getNickName() + ": " + message + "\n") != -1;
                     }
                     else
-                        sendReturn &= sendMsg(client->getSocket(), error(ERR_NOSUCHNICK, client->getNickName())) != -1;
+                        sendReturn &= sendMsg(client->getSocket(), ERR_NOSUCHNICK(client->getNickName(), receiver)) != -1;
                 }
             }
             return sendReturn;
         }
  
+        // std::string getTargets(std::vector<std::string> &toSend) {
+        //     std::string targets;
+        //     for (size_t i = 0; i < toSend.size(); i++) {
+        //         targets += toSend[i];
+        //         if ()
+        //     }
+        //     return targets;
+        // }
+
         void    executePrivmsg() {
             std::string messageTosSend = "";
             size_t msgIdx = originCmd.find(':');
@@ -234,11 +196,11 @@ class Command
             // std::cout << '\n';
             int sendReturn = 1;
             if (toSend.empty())
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NORECIPIENT ,client->getNickName())) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NORECIPIENT(client->getNickName())) != -1;
             if (messageTosSend.empty())
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_NOTEXTTOSEND, client->getNickName())) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_NOTEXTTOSEND(client->getNickName())) != -1;
             if (toSend.size() > 10)
-                sendReturn &= sendMsg(client->getSocket(), error(ERR_TOOMANYTARGETS, client->getNickName())) != -1;
+                sendReturn &= sendMsg(client->getSocket(), ERR_TOOMANYTARGETS(client->getNickName(), originCmd, 0)) != -1;
             sendReturn &= sendPrivMessage(toSend, messageTosSend);
             if (!sendReturn)
                 std::cerr << "Error occurs while sending message to the client\n";
@@ -275,7 +237,7 @@ class Command
             else {
                 if (cmdIdx >= 3) {
                     if (!client->isAlreadyRegistred()) {
-                        if (sendMsg(client->getSocket(), error(ERR_NOTREGISTERED, client->getNickName().empty() ? "*" : client->getNickName())) == -1)
+                        if (sendMsg(client->getSocket(), ERR_NOTREGISTERED(((client->getNickName().empty()) ? "*" : client->getNickName()))) == -1)
                             std::cerr << "Error occurs while sending message to the client\n";
                         return ;
                     }
@@ -284,32 +246,3 @@ class Command
             }
         }
 };
-
-// what I need,  fdToClient
-// need the serverData
-
-
-//client class : username, nickname, isRegistred, isAuthenticated 
-// PASS :
-// 	syntax : PASS passwd
-// 	errors: -ERR_NEEDMOREPARAMS
-// 		    -ERR_ALREADYREGISTRED
-// 		    -ERR_PASSWDMISMATCH
-	
-
-// NICK: 
-// 	syntax : NICK nickname
-// 		ERR_NONICKNAMEGIVEN
-// 		ERR_ERRONEUSNICKNAME. (invalid nickname characters)
-// 		ERR_NICKNAMEINUSE(already in use by someone else)
-// 		ERR_NOTREGISTERED not authenticated yet(didnt make PASS yet)
-// USER :
-// 		ERR_NEEDMOREPARAMS
-// 		ERR_ALREADYREGISTRED
-
-// JOIN  :
-// 	syntax : JOIN <channel>{,<channel>} [<key>{,<key>}]
-// 	examples:
-// 		JOIN #example,#example2 
-// 		JOIN #example secretpassword
-// 		JOIN  #example,#example2 secretpassword1,secretpassword2
